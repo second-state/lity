42abd97dfe214383d6ec743c18c97da1ed12cec9
Yi Huang <yi@skymizer.com>
2018-08-02 08:20:52 +0000

    [Test] Functions using `factInsert`, `factDelete`, `fireAllRules` should not be declared `pure`.

--- test/libsolidity/syntaxTests/rule/view_pure_fact_insert.sol
+++ test/libsolidity/syntaxTests/rule/view_pure_fact_insert.sol
@@ -4,9 +4,13 @@ contract C {
         bool eligible;
         address addr;
     }
-    function addPerson(int age) public view {
+    function addPersonView(int age) public view {
+        factInsert Person(age, false, address(0));
+    }
+    function addPersonPure(int age) public pure {
         factInsert Person(age, false, address(0));
     }
 }
 // ----
-// Warning: (155-196): Function declared as view, but this expression (potentially) modifies the state and thus requires non-payable (the default) or payable.
+// Warning: (159-200): Function declared as view, but this expression (potentially) modifies the state and thus requires non-payable (the default) or payable.
+// TypeError: (266-307): Function declared as pure, but this expression (potentially) modifies the state and thus requires non-payable (the default) or payable.
