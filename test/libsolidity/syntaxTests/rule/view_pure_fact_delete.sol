f7651d171528fdef0409e0b0190d4d66ce1e2b6d
Yi Huang <yi@skymizer.com>
2018-08-02 08:20:52 +0000

    [Test] Functions using `factInsert`, `factDelete`, `fireAllRules` should not be declared `pure`.

--- test/libsolidity/syntaxTests/rule/view_pure_fact_delete.sol
+++ test/libsolidity/syntaxTests/rule/view_pure_fact_delete.sol
@@ -1,7 +1,11 @@
 contract C {
-    function deletePerson() public view {
+    function deletePersonView() public view {
+        factDelete uint256(0);
+    }
+    function deletePersonPure() public pure {
         factDelete uint256(0);
     }
 }
 // ----
-// Warning: (63-84): Function declared as view, but this expression (potentially) modifies the state and thus requires non-payable (the default) or payable.
+// Warning: (67-88): Function declared as view, but this expression (potentially) modifies the state and thus requires non-payable (the default) or payable.
+// TypeError: (150-171): Function declared as pure, but this expression (potentially) modifies the state and thus requires non-payable (the default) or payable.
--- test/libsolidity/syntaxTests/rule/view_pure_fire_all_rules.sol
+++ test/libsolidity/syntaxTests/rule/view_pure_fire_all_rules.sol
@@ -1,7 +1,11 @@
 contract C {
-    function pay() public view {
+    function payView() public view {
+        fireAllRules;
+    }
+    function payPure() public pure {
         fireAllRules;
     }
 }
 // ----
-// Warning: (54-66): Function declared as view, but this expression (potentially) modifies the state and thus requires non-payable (the default) or payable.
+// Warning: (58-70): Function declared as view, but this expression (potentially) modifies the state and thus requires non-payable (the default) or payable.
+// TypeError: (123-135): Function declared as pure, but this expression (potentially) modifies the state and thus requires non-payable (the default) or payable.
