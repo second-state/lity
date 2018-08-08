8041fde157275ea91e409faf8c7c4d5459eb05cf
Yi Huang <yi@skymizer.com>
2018-08-08 12:39:21 +0000

    [Test] Add more lines to rules runtime test.

--- test/lity/RulesTest.cpp
+++ test/lity/RulesTest.cpp
@@ -32,10 +32,39 @@ protected:
 /// This is a test suite that tests optimised code!
 BOOST_FIXTURE_TEST_SUITE(LityRules, RulesTestFramework)
 
-BOOST_AUTO_TEST_CASE(creation)
+BOOST_AUTO_TEST_CASE(numberOfPeople)
 {
 	deployContract();
+	// empty list
 	BOOST_REQUIRE(callContractFunction("numberOfPeople()") == encodeArgs(u256(0)));
+}
+
+BOOST_AUTO_TEST_CASE(isEligible)
+{
+	deployContract();
+	// not in list, not eligible
+	BOOST_REQUIRE(callContractFunction("isEligible()") == encodeArgs(false));
+}
+
+BOOST_AUTO_TEST_CASE(addPerson)
+{
+	deployContract();
+	u256 balance = 10 * ether;
+	sendEther(account(1), balance);
+
+	// add account(1) to list
+	BOOST_REQUIRE(callContractFunctionFrom(1, "addPerson(uint256)", u256(18)) == encodeArgs());
+	balance -= gasCost();
+	BOOST_CHECK_EQUAL(balanceAt(account(1)), balance);
+
+	// list size is 1
+	BOOST_REQUIRE(callContractFunction("numberOfPeople()") == encodeArgs(u256(1)));
+	BOOST_CHECK_EQUAL(balanceAt(account(1)), balance);
+
+	// in list, eligible
+	BOOST_REQUIRE(callContractFunctionFrom(1, "isEligible()") == encodeArgs(true));
+	balance -= gasCost();
+	BOOST_CHECK_EQUAL(balanceAt(account(1)), balance);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
