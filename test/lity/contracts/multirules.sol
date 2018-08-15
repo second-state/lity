9c236199b0d249727d8dbe9a401eaed1844e4924
Yi Huang <yi@skymizer.com>
2018-08-15 05:48:47 +0000

    [Test] Add multi-rules test-case to runtime test.

---
+++ test/lity/contracts/multirules.sol
@@ -0,0 +1,33 @@
+contract C {
+    struct Person {
+        uint age;
+        uint balance;
+    }
+
+    rule "myRule1" when {
+        p: Person(age < 18);
+    } then {
+        p.balance += 10;
+    }
+
+    rule "myRule2" when {
+        p: Person(age > 20);
+    } then {
+        p.balance += 7;
+    }
+
+    mapping (address => uint) addr2idx;
+    Person[] ps;
+
+    function numberOfPeople() public view returns (uint) {
+        return ps.length;
+    }
+    function balance(uint i) public view returns (uint) {
+        return ps[i].balance;
+    }
+    function addPerson(uint age) public {
+        ps.push(Person(age, 0));
+        addr2idx[msg.sender] = factInsert ps[ps.length-1];
+        fireAllRules;
+    }
+}
