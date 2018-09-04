d10f1cc2e12a8fa0c3c7a89e80904f4159632f41
Yi Huang <yi@skymizer.com>
2018-09-04 04:05:16 +0000

    [Docs] Add logo.

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,9 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo_only': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -96,7 +98,7 @@ html_static_path = ['_static']
 # 'searchbox.html']``.
 #
 # html_sidebars = {}
-
+html_logo = '_static/images/logo@white.png'
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
