38950c291d2a09e5c4ee08577eb79c1ccbb90cf0
mingchuan <mingc@skymizer.com>
2018-09-09 19:46:15 +0800

    [Rule] Add basic runtime test for update-clause

---
+++ test/lity/UpdateRulesTest.cpp
@@ -0,0 +1,63 @@
+#include <boost/test/unit_test.hpp>
+#include <test/lity/ContractExecutionFramework.h>
+
+using namespace std;
+using namespace dev::test;
+
+namespace dev
+{
+namespace solidity
+{
+namespace test
+{
+
+static unique_ptr<bytes> s_compiledContract;
+
+class UpdateRulesTestFramework: public ContractExecutionFramework
+{
+protected:
+	void deployContract()
+	{
+		if (!s_compiledContract)
+			s_compiledContract.reset(new bytes(compileContractFile(
+				"test/lity/contracts/updaterules.sol",
+				"C")));
+
+		bytes args = encodeArgs();
+		sendMessage(*s_compiledContract + args, true);
+		BOOST_REQUIRE(!m_output.empty());
+	}
+};
+
+/// This is a test suite that tests optimised code!
+BOOST_FIXTURE_TEST_SUITE(LityRules, UpdateRulesTestFramework)
+
+BOOST_AUTO_TEST_CASE(updateBasicTest)
+{
+	deployContract();
+	int const numUser = 10;
+	u256 balance[numUser+1];
+	int age[numUser+1];
+	srand(42);
+	for (int i = 1; i <= numUser; i++)
+	{
+		balance[i] = 1000 * ether;
+		sendEther(account(i), balance[i]);
+		BOOST_REQUIRE(callContractFunctionFrom(i, "addPerson(uint256)", u256((age[i]=1+rand()%10))) == encodeArgs());
+		balance[i] -= gasCost();
+		BOOST_CHECK_EQUAL(balanceAt(account(i)), balance[i]);
+	}
+
+	// pay money to persons in working memory
+	sendEther(m_contractAddress, u256(10000) * numUser);
+	BOOST_REQUIRE(callContractFunction("pay()") == encodeArgs(true));
+
+	for (int i = 1; i <= numUser; i++)
+		BOOST_CHECK_EQUAL(balanceAt(account(i)), balance[i] + 10*age[i]);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}
+}
+} // end namespaces
---
+++ test/lity/contracts/updaterules.sol
@@ -0,0 +1,27 @@
+contract C {
+    struct Person {
+        uint age;
+        address addr;
+    }
+
+    rule "updateTest" when {
+        p: Person(age > 0);
+    } then {
+        p.addr.transfer(10);
+        p.age--;
+        update p;
+    }
+
+    mapping (address => uint) addr2idx;
+    Person[] ps;
+
+    function addPerson(uint age) public {
+        ps.push(Person(age, msg.sender));
+        addr2idx[msg.sender] = factInsert ps[ps.length-1];
+    }
+    function pay() public returns (bool) {
+        fireAllRules;
+        return true;
+    }
+    function () public payable { }
+}
