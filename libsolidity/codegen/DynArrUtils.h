150cd66c71abb9d7744bb1da5a4e8f92e271f723
mingchuan <mingc@skymizer.com>
2018-09-09 19:41:19 +0800

    [Rule] Make dynamic array forEachDo breakable

--- libsolidity/codegen/DynArrUtils.h
+++ libsolidity/codegen/DynArrUtils.h
@@ -21,7 +21,7 @@ using TypePointer = std::shared_ptr<Type const>;
  *   dataPtr points to memory address of the data
  *   len is the length of this array (the number of elements)
  *   cap is the capacity allocated of this array
- * if len is greater than cap after push, 
+ * if len is greater than cap after push,
  * 2*cap-size mem space would be allocated
  * and data would be copied to that space
  */
@@ -42,7 +42,7 @@ public:
 	/// Stack pre : referrence
 	/// Stack post:
 	void popItem();
-	
+
 	/// Stack pre : referrence
 	/// Stack post: len'
 	void incrLen();
@@ -64,10 +64,14 @@ public:
 	/// For each element do f.
 	/// Stack pre : reference
 	/// Stack post:
-	/// Stack state of f:
+	/// Stack state of f (if not breakable):
 	///   Stack pre : elmtMemAddr
 	///   Stack post:
-	void forEachDo(std::function<void(CompilerContext&)> f);
+	/// Stack state of f (if breakable):
+	///   Stack pre : elmtMemAddr
+	///   Stack post: breakOrNot
+	void forEachDo(std::function<void(CompilerContext&)> f, bool breakable = false);
+
 private:
 
 	/// Stack pre : referrence
