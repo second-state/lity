1d0c0107dd34b4731f8fdfeb13e0f755f1f367fa
mingchuan <mingc@skymizer.com>
2018-10-19 04:02:46 +0800

    [Rule] Add basic runtime tests for no-loop and lock-on-active

---
+++ test/lity/contracts/noloop1.sol
@@ -0,0 +1,37 @@
+pragma lity ^1.2.0;
+
+contract C {
+
+    struct S {
+        uint256 value;
+    }
+
+    S x;
+
+    rule "increaseValue" salience 0 no_loop true
+    when {
+        s: S();
+    } then {
+        s.value++;
+        update s;
+    }
+
+    rule "increaseValue2" salience 10 no_loop true
+    when {
+        s: S();
+    } then {
+        s.value += 2;
+        update s;
+    }
+
+    function calc() public returns (uint256) {
+        x.value = 1;
+        factInsert x;
+        fireAllRules; // should not terminate
+        return x.value;
+    }
+
+    function () public payable { }
+}
+
+// ----
---
+++ test/lity/contracts/noloop2.sol
@@ -0,0 +1,37 @@
+pragma lity ^1.2.0;
+
+contract C {
+
+    struct S {
+        uint256 value;
+    }
+
+    S x;
+
+    rule "increaseValue" salience 0 no_loop true
+    when {
+        s: S();
+    } then {
+        s.value++;
+        update s;
+    }
+
+    rule "increaseValue2" salience 10 lock_on_active true
+    when {
+        s: S();
+    } then {
+        s.value += 2;
+        update s;
+    }
+
+    function calc() public returns (uint256) {
+        x.value = 1;
+        factInsert x;
+        fireAllRules;
+        return x.value; // should be 4
+    }
+
+    function () public payable { }
+}
+
+// ----
