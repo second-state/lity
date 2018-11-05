f6c8b886b6cdd479468f9bd57f31143e07a8b31b
Yi Huang <yi@skymizer.com>
2018-11-05 10:02:34 +0000

    [Docs] Enable Sphinx TODO block extension.

--- docs/conf.py
+++ docs/conf.py
@@ -39,8 +39,10 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.mathjax"
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
 ]
+todo_include_todos = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
