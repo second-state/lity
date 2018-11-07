a26196c637170bce68dbf36ca23f9946c7f6bc22
Yi Huang <yi@skymizer.com>
2018-11-07 08:20:45 +0000

    [Test] Update test expectations of error messages.
    - denominations/fixed_point_division
    - types/rational_number_div_limit

--- test/libsolidity/syntaxTests/types/rational_number_div_limit.sol
+++ test/libsolidity/syntaxTests/types/rational_number_div_limit.sol
@@ -6,4 +6,4 @@ contract c {
 }
 // ----
 // TypeError: (71-92): Operator / not compatible with types rational_const 1 / 5221...(1225 digits omitted)...5168 and int_const 5221...(1225 digits omitted)...5168
-// TypeError: (71-92): Type rational_const 1 / 5221...(1225 digits omitted)...5168 is not implicitly convertible to expected type int256. Try converting to type ufixed8x80 or use an explicit conversion.
+// TypeError: (71-92): Type rational_const 1 / 5221...(1225 digits omitted)...5168 is not implicitly convertible to expected type int256.
