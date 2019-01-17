4136d80997f7d372add6abcee876e6dbd553b548
Yi Huang <yi@skymizer.com>
2019-01-17 08:51:02 +0000

    [Test] Add fixed-point types that should not cause declaration error.

---
+++ test/libsolidity/syntaxTests/types/fixed_number_limit.sol
@@ -0,0 +1,22 @@
+pragma lity ^1.2.6;
+contract C {
+    function x() public pure {
+      ufixed8x1     u8;   u8;
+      ufixed16x1   u16;  u16;
+      ufixed32x1   u32;  u32;
+      ufixed64x1   u64;  u64;
+      ufixed128x1 u128; u128;
+      ufixed256x1 u256; u256;
+      ufixed256x88   u;    u;
+      ufixed256x256  v;    v;
+
+      fixed8x1     f8;   f8;
+      fixed16x1   f16;  f16;
+      fixed32x1   f32;  f32;
+      fixed64x1   f64;  f64;
+      fixed128x1 f128; f128;
+      fixed256x1 f256; f256;
+      fixed256x88   f;    f;
+      fixed256x256  g;    g;
+    }
+}
